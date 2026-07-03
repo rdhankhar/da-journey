7f08d9f5b1740aded8bb5fc0ac54850642172fa3
rdhankhar <rahulqwert1212@gmail.com>
2026-07-04 00:09:30 +0530

    Completed Python Part 1

---
+++ python/python_part1.py
@@ -0,0 +1,106 @@
+# Airthmetic operatore
+# a = 10 
+# b = 5
+
+# print("sum is : ", a + b)
+# print("difference is : ", a - b)
+# print("product is : ", a * b)
+# print("division is : ", a / b)
+# print("modulus is : ", a % b)
+# print("squared  is : ", a ** b)
+
+# Assignment operator
+
+# a = 20
+# print("original value is :", a)
+
+# a += 10
+# print("addition is :", a)
+
+# a -= 10
+# print("subtraction is :", a)
+
+# a *= 10
+# print("multiplication is :", a)
+
+# a %= 10
+# print("modulus is :", a)
+
+# a /= 10
+# print("division is :", a)
+
+# a **= 10
+# print("powered is :", a)
+
+# Realtional operator
+
+# a = 20 
+# b = 30 
+
+# print ( a == b )
+# print ( a != b )
+# print ( a >= b )
+# print ( a <= b )
+# print ( a > b )
+# print ( a < b )
+
+# Logical operator
+
+# a = 20
+# b = 30
+
+# print(not (True))
+
+
+# val1 = True
+# val2 = False
+
+# print("And operator : ", val1 and val2)
+# print("Or operator : ", val1 or val2)
+
+# Input
+
+# name = input("enter your name : ")
+# age  = input("enter your age : ")
+# print("You entered ", age )
+# print("welcome  ", name )
+
+# val = input("enter the value : ")
+# print(type (val), val )
+
+# name = input("enter name : ")
+# age  = int (input("enter age : "))
+# marks =float (input("enter marks : "))
+
+# print("name : ", name )
+# print("age : ", age )
+# print("marks : ", marks )
+
+# Practice questions
+
+# a = int(input("enter value of a : "))
+# b =int( input("enter value of b  : "))
+
+
+# print(" sum is : ", a + b )
+
+# square of a number 
+
+# side = float(input("enter side of a square : "))
+
+# print("Area is : ", side * side )
+
+# Average of two numbers
+
+# f = float(input("enter first value : "))
+# s = float( input("enter second value : "))
+
+# print("average is : ", (f + s)/2 )
+
+# logical question
+
+a = int(input("enter first value : "))
+
+b = int(input("enter second value : "))
+
+print( a>= b)
